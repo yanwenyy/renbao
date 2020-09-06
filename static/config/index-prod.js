3bfcaf694f7a36ead0944433499ea5ce6d77d032
yanwenyy <18738390058>
2020-09-06 22:13:44 +0800

    加二维码审核

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,8 +5,8 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://27.188.74.31:9080/jinding-back/';
-  window.SITE_CONFIG['wsUrl'] = 'ws://27.188.74.31:9080/jinding-back/imserver/';
+  window.SITE_CONFIG['baseUrl'] = 'http://172.16.48.105:9080/tangshan-back/';
+  window.SITE_CONFIG['wsUrl'] = 'ws://172.16.48.105:9080/tangshan-back/';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
