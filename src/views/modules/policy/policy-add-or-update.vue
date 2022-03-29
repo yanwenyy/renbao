0191253a54968bafcf1cda82d1c55a082993409a
yanwenyy <18738390058>
2022-03-29 17:46:30 +0800

    bug修改

--- src/views/modules/policy/policy-add-or-update.vue
+++ src/views/modules/policy/policy-add-or-update.vue
@@ -39,7 +39,8 @@
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="visible = false">取消</el-button>
-      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+      <!--<el-button type="primary" @click="dataFormSubmit()">确定</el-button>-->
+      <el-button type="primary" @click="">确定</el-button>
     </span>
   </el-dialog>
 </template>
